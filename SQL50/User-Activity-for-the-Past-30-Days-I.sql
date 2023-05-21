b2978d2a85df37f6571e085eafe37efe2cd7ead5
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-21 10:47:55 +0300

    Update User-Activity-for-the-Past-30-Days-I.sql

--- SQL50/User-Activity-for-the-Past-30-Days-I.sql
+++ SQL50/User-Activity-for-the-Past-30-Days-I.sql
@@ -17,3 +17,8 @@ Write an SQL query to find the daily active user count for a period of 30 days e
 A user was active on someday if they made at least one activity on that day.
 Return the result table in any order.
 */
+
+SELECT activity_date day, COUNT(DISTINCT user_id) active_users
+FROM Activity
+WHERE activity_date between '2019-06-28' and '2019-07-27'
+GROUP BY activity_date
