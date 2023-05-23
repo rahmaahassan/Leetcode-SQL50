ef98843b2385b7125d2cd5fe240b3eea0cb15a56
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-23 07:14:54 +0300

    Create Biggest-Single-Number.sql

---
+++ SQL50/Biggest-Single-Number.sql
@@ -0,0 +1,14 @@
+/*
+Table: MyNumbers
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+There is no primary key for this table. It may contain duplicates.
+Each row of this table contains an integer.
+
+A single number is a number that appeared only once in the MyNumbers table.
+Write an SQL query to report the largest single number. If there is no single number, report null.
+*/
+
