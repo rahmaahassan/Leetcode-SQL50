fdf3ec4aac256834c063ac93250dd90831e2768c
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-23 07:20:11 +0300

    Create Triangle-Judgement.sql

---
+++ SQL50/Triangle-Judgement.sql
@@ -0,0 +1,16 @@
+/*
+Table: Triangle
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+(x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+Write an SQL query to report for every three line segments whether they can form a triangle.
+Return the result table in any order.
+*/
+
