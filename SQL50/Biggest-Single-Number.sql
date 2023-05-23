b273d6e99ef061f1bfea2a66036f6183297aafd5
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-23 07:15:10 +0300

    Update Biggest-Single-Number.sql

--- SQL50/Biggest-Single-Number.sql
+++ SQL50/Biggest-Single-Number.sql
@@ -12,3 +12,11 @@ A single number is a number that appeared only once in the MyNumbers table.
 Write an SQL query to report the largest single number. If there is no single number, report null.
 */
 
+SELECT MAX(num) num
+FROM MyNumbers
+WHERE num in (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+)
