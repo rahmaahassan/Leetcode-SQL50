534ed148075c051bb87dc734b8d8e1269b6b9cd5
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-24 08:14:11 +0300

    Update Employees-Whose-Manager-Left-the-Company.sql

--- SQL50/Employees-Whose-Manager-Left-the-Company.sql
+++ SQL50/Employees-Whose-Manager-Left-the-Company.sql
@@ -15,3 +15,11 @@ Write an SQL query to report the IDs of the employees whose salary is strictly l
 When a manager leaves the company, their information is deleted from the Employees table, but the reports still have their manager_id set to the manager that left.
 Return the result table ordered by employee_id.
 */
+
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000 AND manager_id NOT IN (
+  SELECT employee_id
+  FROM Employees
+) 
+ORDER BY employee_id
