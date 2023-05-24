c9e5c737f76333405acf88f1b2598b875ea011ff
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-24 07:31:51 +0300

    Update Game-Play-Analysis-IV.sql

--- SQL50/Game-Play-Analysis-IV.sql
+++ SQL50/Game-Play-Analysis-IV.sql
@@ -15,4 +15,3 @@ Each row is a record of a player who logged in and played a number of games (pos
 Write an SQL query to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. 
 In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
 */
-
