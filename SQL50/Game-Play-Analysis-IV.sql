f7863bbc81068d5866b3a923d4234640815409b4
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-24 07:31:59 +0300

    Update Game-Play-Analysis-IV.sql

--- SQL50/Game-Play-Analysis-IV.sql
+++ SQL50/Game-Play-Analysis-IV.sql
@@ -15,3 +15,13 @@ Each row is a record of a player who logged in and played a number of games (pos
 Write an SQL query to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. 
 In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
 */
+
+WITH CTE AS (
+    SELECT player_id, MIN(event_date) first_login
+    FROM Activity
+    GROUP BY player_id
+)
+
+SELECT ROUND(1.0 * COUNT(a.player_id) / (SELECT COUNT(DISTINCT player_id) * 1.0 FROM Activity), 2) fraction
+FROM CTE c LEFT JOIN Activity a
+ON c.player_id = a.player_id AND a.event_date = DATEADD(day, 1, first_login)
