e2bce052f1dc581920b8d29c245a08eb2b475625
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-26 09:47:21 +0300

    Update The-Number-of-Employees-Which-Report-to-Each-Employee.sql

--- SQL50/The-Number-of-Employees-Which-Report-to-Each-Employee.sql
+++ SQL50/The-Number-of-Employees-Which-Report-to-Each-Employee.sql
@@ -15,3 +15,9 @@ For this problem, we will consider a manager an employee who has at least 1 othe
 Write an SQL query to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
 Return the result table ordered by employee_id.
 */
+
+SELECT e.employee_id, e.name, COUNT(m.employee_id) reports_count, ROUND(AVG(m.age * 1.0), 0) average_age
+FROM Employees e INNER JOIN Employees m
+ON e.employee_id = m.reports_to
+GROUP BY e.employee_id, e.name
+ORDER BY e.employee_id
