46a21a18b79be383c1b34d2c4e507bbedb5ac509
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-28 08:28:25 +0300

    Update Product-Price-at-a-Given-Date.sql

--- SQL50/Product-Price-at-a-Given-Date.sql
+++ SQL50/Product-Price-at-a-Given-Date.sql
@@ -13,3 +13,12 @@ Each row of this table indicates that the price of some product was changed to a
 Write an SQL query to find the prices of all products on 2019-08-16. Assume the price of all products before any change is 10.
 Return the result table in any order.
 */
+
+SELECT DISTINCT product_id, 
+                COALESCE(
+                  (SELECT TOP(1) new_price
+                  FROM Products p
+                  WHERE Products.product_id = p.product_id 
+                  AND p.change_date <= '2019-08-16'
+                  ORDER BY p.change_date DESC), 10) price
+FROM Products
