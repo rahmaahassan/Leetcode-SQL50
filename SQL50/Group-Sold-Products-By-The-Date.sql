7dfe8838ecafbcb1b34eff1665e291f83ad5bfbd
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-29 08:26:33 +0300

    Create Group-Sold-Products-By-The-Date.sql

---
+++ SQL50/Group-Sold-Products-By-The-Date.sql
@@ -0,0 +1,16 @@
+/*
+Table Activities:
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key for this table, it may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
+
+Write an SQL query to find for each date the number of different products sold and their names.
+The sold products names for each date should be sorted lexicographically.
+Return the result table ordered by sell_date.
+*/
+
