b6b83895ac568f313967936ba51b6f836ad5e814
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-29 08:46:49 +0300

    Update Department-Top-Three-Salaries.sql

--- SQL50/Department-Top-Three-Salaries.sql
+++ SQL50/Department-Top-Three-Salaries.sql
@@ -29,3 +29,12 @@ Write an SQL query to find the employees who are high earners in each of the dep
 Return the result table in any order.
 */
 
+WITH cte AS(
+    SELECT e.name AS Employee, e.salary AS Salary, d.name AS Department , 
+        DENSE_RANK() OVER (PARTITION BY d.name ORDER BY salary DESC) AS RANKK
+    FROM Employee e
+    JOIN Department d ON e.departmentId = d.id
+)
+SELECT Department, Employee, Salary
+FROM cte
+WHERE RANKK <= 3
