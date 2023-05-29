0e7b59ce73361e34fddef9ee1309ed18e483c766
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-29 08:26:58 +0300

    Update Group-Sold-Products-By-The-Date.sql

--- SQL50/Group-Sold-Products-By-The-Date.sql
+++ SQL50/Group-Sold-Products-By-The-Date.sql
@@ -14,3 +14,8 @@ The sold products names for each date should be sorted lexicographically.
 Return the result table ordered by sell_date.
 */
 
+SELECT sell_date, count(product) num_sold,
+       string_agg(product, ',') products
+FROM (SELECT DISTINCT * from Activities) tabel1
+GROUP BY sell_date
+ORDER BY sell_date
