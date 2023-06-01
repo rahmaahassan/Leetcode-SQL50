884a0bd37c4aed633af519447c928878cd3a8292
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-06-01 11:03:49 +0300

    Create Movie-Rating.sql

---
+++ SQL50/Movie-Rating.sql
@@ -0,0 +1,38 @@
+/*
+Table: Movies
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| movie_id      | int     |
+| title         | varchar |
++---------------+---------+
+movie_id is the primary key for this table.
+title is the name of the movie.
+ 
+Table: Users
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| name          | varchar |
++---------------+---------+
+user_id is the primary key for this table.
+ 
+Table: MovieRating
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| movie_id      | int     |
+| user_id       | int     |
+| rating        | int     |
+| created_at    | date    |
++---------------+---------+
+(movie_id, user_id) is the primary key for this table.
+This table contains the rating of a movie by a user in their review.
+created_at is the user's review date. 
+ 
+Write an SQL query to:
+
+Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+*/
