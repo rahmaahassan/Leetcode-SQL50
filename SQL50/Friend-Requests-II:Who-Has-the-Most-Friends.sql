6532bbea65cbd72e7ae9ff761f517c0b62eeb859
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-06-01 10:17:46 +0300

    Update Friend-Requests-II:Who-Has-the-Most-Friends.sql

--- SQL50/Friend-Requests-II:Who-Has-the-Most-Friends.sql
+++ SQL50/Friend-Requests-II:Who-Has-the-Most-Friends.sql
@@ -1,1 +1,15 @@
 /*
+Table: RequestAccepted
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| requester_id   | int     |
+| accepter_id    | int     |
+| accept_date    | date    |
++----------------+---------+
+(requester_id, accepter_id) is the primary key for this table.
+This table contains the ID of the user who sent the request, the ID of the user who received the request, and the date when the request was accepted.
+ 
+Write an SQL query to find the people who have the most friends and the most friends number.
+The test cases are generated so that only one person has the most friends.
+*/
