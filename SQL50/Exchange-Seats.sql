f6c804a4258706299f4e7a4cb50d0e3e15101ac2
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-06-14 14:00:25 +0300

    Update Exchange-Seats.sql

--- SQL50/Exchange-Seats.sql
+++ SQL50/Exchange-Seats.sql
@@ -13,3 +13,11 @@ id is a continuous increment.
 Write an SQL query to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
 Return the result table ordered by id in ascending order.
 */
+
+SELECT id, ISNULL(CASE
+           WHEN id % 2 = 1 THEN LEAD(student, 1) OVER(ORDER BY id)
+           ELSE LAG(student, 1) OVER(ORDER BY id) END,
+           student) student
+            
+FROM Seat
+ORDER BY id
