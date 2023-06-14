4e7e1798b0820b8680f44100110e2960d38e67c7
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-06-14 14:00:03 +0300

    Create Exchange-Seats.sql

---
+++ SQL50/Exchange-Seats.sql
@@ -0,0 +1,15 @@
+/*
+Table: Seat
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the name and the ID of a student.
+id is a continuous increment.
+ 
+Write an SQL query to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+Return the result table ordered by id in ascending order.
+*/
